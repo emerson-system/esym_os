12f6f0f2f7c16bb5ee6f916c04dd672a238dbf5b
emerson-system <emersonsilva81@hotmail.com>
2023-08-16 12:17:44 -0300

    Commit do head

---
+++ templates/element/Layout/head.php
@@ -0,0 +1,12 @@
+l<?php
+/**
+ * @var \App\View\AppView $this
+ * @var array $params
+ * @var string $message
+ */
+    use Cake\Core\Configure;
+?>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content+"IE=edge">
+
